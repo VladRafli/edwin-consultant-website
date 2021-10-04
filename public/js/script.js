91b90364987e5faf329c79188d2d249bb3e48ebb
Rafli Athala Jaskandi <rafli.jaskandi@gmail.com>
2021-10-04 10:11:22 +0700

    Major overhaul:
    - Give 5 second timeout for first landing redirection (This should solve Search Engine Indexing problem I found)
    
    - Added indonesian language page
    
    - Overhaul on page source code
    
    - Merge code from 'vcentsu-dev2' - https://github.com/VladRafli/edwin-consultant-website/tree/vcentsu-dev2

--- public/js/script.js
+++ public/js/script.js
@@ -15,14 +15,52 @@ $('#lang-select').on('change', () => {
             // Debug data
             console.log(localStorage.getItem('language'));
             // Un-comment on full server implmentation
-            // window.location.href = '/en';
+            window.location.href = `${window.location.origin}/en`;
             break;
         case 'id':
             localStorage.setItem('language', 'id');
             // Debug data
             console.log(localStorage.getItem('language'));
             // Un-comment on full server implmentation
-            // window.location.href = '/id';
+            window.location.href = `${window.location.origin}/id`;
             break;
     }
 });
+/**
+ * * This is for landing backdrop to be responsive to element above it.
+ **/
+ $(window).on('load', () => {
+    $('.backdrop').attr('style', `height: calc(${$('.hero').innerHeight()}px + 15vh + 2.5rem + 8rem)`);
+});
+$(window).on('resize', () => {
+    $('.backdrop').attr('style', `height: calc(${$('.hero').innerHeight()}px + 15vh + 2.5rem + 8rem)`);
+});
+/**
+ * * This is for Navbar Behaviour
+ **/
+$(window).on('scroll', () => {
+    if (window.innerWidth > 426) {
+        if (window.pageYOffset >= 10) {
+            $('.navbar').addClass('navbar-contrast');
+            $('.menu-button').addClass('menu-button-contrast');
+        } else if (window.pageYOffset < 10) {
+            $('.navbar').removeClass('navbar-contrast');
+            $('.menu-button').removeClass('menu-button-contrast');
+        }
+    }
+    if (window.pageYOffset >= 10) {
+        $('.mobile-nav').addClass('mobile-nav-contrast');
+    } else if (window.pageYOffset < 10) {
+        $('.mobile-nav').removeClass('mobile-nav-contrast');
+    }
+});
+// Create dynamic href link
+if (localStorage.getItem('language') === 'en') {
+    $('.hero-buttons > #form').attr('href', `${window.location.origin}/en/contact`);
+    $('.logo > a').attr('href', `${window.location.origin}/en`);
+    $('#contact-footer').attr('href', `${window.location.origin}/en/contact`);
+} else if (localStorage.getItem('language') === 'id') {
+    $('.hero-buttons > #form').attr('href', `${window.location.origin}/id/contact`);
+    $('.logo > a').attr('href', `${window.location.origin}/id`);
+    $('#contact-footer').attr('href', `${window.location.origin}/id/contact`);
+}
